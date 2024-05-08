e82f95388295721733489618471b3d57ae0c6ffc
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-05-08 11:41:08 +0100

    added the final update

---
+++ puppy.go
@@ -0,0 +1,12 @@
+package puppy
+
+// Bark returns the sound a puppy makes when barking.
+// No parameters.
+// Returns a string.
+func Bark() string {
+	return "ruff ruff"
+}
+
+func Barks() string {
+	return "ruff ruff ruff"
+}
